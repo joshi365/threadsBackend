6238442a53a1fc206972579ecd8814a11b0bf9b4
Sumeet Joshi <sumeetjoshi@Sumeets-MacBook-Air.local>
2024-11-18 01:25:53 +0530

    cors error

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from "./db/connectDb.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js"
 import postRoutes from "./routes/postRoutes.js"
+import cors from "cors";
 
 
 dotenv.config()
@@ -18,6 +19,11 @@ const PORT = process.env.PORT || 5001
 app.use(express.json()); /// to parse json data in req body
 app.use(express.urlencoded({extended:true})); // to parse from data into req body
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:3000", // Replace with your frontend's origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    credentials: true // Include credentials if needed (e.g., cookies)
+}));
 //app.use(helmet({ hsts: false }));
 
 //Routes
